94f80f433b4ef84e7a428d24d1aa170493e2174b
Miroslaw <miroslawklus@gmail.com>
2022-07-24 14:48:55 +0200

    добавил код

---
+++ 2sem.laba11.cpp
@@ -0,0 +1,36 @@
+﻿//Дан класс, который умеет генерировать код на разных языках.Предложите рефакторинг кода с учетом того, что количество языков будет расширяться.
+
+#include <iostream>
+#include "inc/someFunctions.hpp"
+#include "inc/php.hpp"
+#include "inc/cPlusPlus.hpp"
+#include "inc/java.hpp"
+#include "inc/generateCode.hpp"
+
+using namespace std;
+
+int main()
+{
+    cout << "===== c++ =====" << endl;
+    CodeGenerator* cppCode = generateCode(C_plus_plus);
+    cout << cppCode->someCodeRelatedThing() << endl;
+    cout << "==========" << endl;
+
+    cout << "===== java =====" << endl;
+    CodeGenerator* javaCode = generateCode(Java);
+    cout << javaCode->someCodeRelatedThing() << endl;
+    cout << "==========" << endl;
+
+    cout << "===== php =====" << endl;
+    CodeGenerator* phpCode = generateCode(PhP);
+    cout << phpCode->someCodeRelatedThing() << endl;
+    cout << "==========" << endl;
+
+    delete cppCode;
+    delete javaCode;
+    delete phpCode;
+
+    return 0;
+}
+
+
